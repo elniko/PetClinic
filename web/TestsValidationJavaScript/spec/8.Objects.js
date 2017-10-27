bc5ea7529610cb1096f4ea21fc0c47768f98d93a
David Junger <junger.d@sfeir.com>
2017-10-27 11:40:01 +0200

    you know nothing, Aegon Targaryen

--- web/TestsValidationJavaScript/spec/8.Objects.js
+++ web/TestsValidationJavaScript/spec/8.Objects.js
@@ -2,14 +2,14 @@ describe('Objects', () => {
 
     it('Objects - 1', () => {
         var result = {};
-        expect(typeof(result)).toBe( _ );
+        expect(typeof result).toBe( _ );
     });
 
     it('Objects - 2', () => {
         var person = {
             // _
         };
-        expect(person.name).toBe('Jean Paul');
+        expect('name' in person).toBe( true );
         expect(person.age).toBe(51);
     });
 
@@ -17,18 +17,11 @@ describe('Objects', () => {
         var person = {};
         Object.assign( person, _ );
         expect(person.name).toBe('Jean Paul');
-        expect(person.age).toBe(51);
-    });
-
-    it('Objects - 4', () => {
-        var person = {};
-        // _
-        // _
-        expect(person.name).toBe('Jean Paul');
+        person // _
         expect(person['phone-number']).toBe('0145254515');
     });
 
-    it('Objects - 5', () => {
+    it('Objects - 4', () => {
         var person = {
             name: "Jean Paul",
             age: 25,
@@ -36,19 +29,22 @@ describe('Objects', () => {
                 return _ ;
             }
         };
-        expect(person.toString()).toBe('My name is Jean Paul and I am 25 years old');
+        expect(person.toString()).toBe('My name is Jean Paul and I am 25 years old.');
     });
 
     it('Objects - 5', () => {
         var person = {
             firstname: "Jon",
             lastname: "Snow",
-            knows: {},
+            knows(something) {
+                return false;
+            },
             // _
         };
         expect(person.fullname).toBe('Jon Snow');
-        person.firstname = "Aegon"
-        expect(person.fullname).toBe('Aegon Snow');
+        person.firstname = "Aegon";
+        person.lastname = "Targaryen";
+        expect(person.fullname).toBe('Aegon Targaryen');
     });
 
 });
